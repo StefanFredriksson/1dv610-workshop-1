94c94600e7dda9f1aa31d4061693001c9a98b8d7
Stefan Fredriksson <sf222pv@student.lnu.se>
2018-09-11 11:13:36 +0200

    First break

--- Logic.php
+++ Logic.php
@@ -1,24 +1,37 @@
 <?php
 class Logic {
-  private $correctWord = array("char1" => 'L', "char2" => 'N', "char3" => 'U');
-  private $guessedWord = array("char1" => '_', "char2" => '_', "char3" => '_');
-  private $num;
+  private $correctWord = array('L', 'N', 'U');
+  private $guessedWord = array('_', '_', '_');
+  private $word;
 
   function joinWord () {
     for ($i = 0; $i < count($this->correctWord); $i++) {
-      $this->num++;
+      $this->word .= $this->guessedWord[$i];
     }
 
-    $this->guessedWord["char2"] = $this->correctWord["char2"];
-    for ($i = 1; $i <= count($this->correctWord); $i++) {
-      $this->num .= $this->guessedWord["char" + $i];
-    }
+    echo $this->word;
+  }
 
-    echo "{$this->num}";
+  function checkIfLetterExists ($letter) {
+    for ($i = 0; $i < count($this->correctWord); $i++) {
+      if ($this->correctWord[$i] == $letter) {
+        $this->guessedWord[$i] = $this->correctWord[$i];
+      }
+    }
+  }
+
+  function listenForGet () {
+    return $_GET["guessedLetter"];
   }
 
   function printHTML() {
-
+    echo "
+    <p>{$this->word}</p>
+    <form method='get'>
+      <input type='text' name='guessedLetter'>
+      <input type='submit'>
+    </form>
+    ";
   }
 }
 ?>
